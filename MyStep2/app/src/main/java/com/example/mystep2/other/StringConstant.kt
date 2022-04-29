05b99d7ce923623f155cb03debe55f2ad7937a75
Mikkel <Vejrup98@gmail.com>
2022-04-29 10:36:42 +0200

    Main

--- MyStep2/app/src/main/java/com/example/mystep2/other/StringConstant.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mystep2.other
-
-const val TAG = "eee"
-const val SHARE = "share"
-const val STEPNUMBER = "step_number"
---
+++ MySteps (Main)/app/src/main/java/com/ix/dm/stepcounter/ui/activity/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.ix.dm.stepcounter.ui.activity
+
+import android.annotation.SuppressLint
+import android.app.Service
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import com.ix.dm.stepcounter.databinding.ActivityMainBinding
+import com.ix.dm.stepcounter.other.*
+import com.ix.dm.stepcounter.util.Constant
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var mBinding: ActivityMainBinding
+
+
+    override fun onResume() {
+        stopService(Intent(this, MyService::class.java))
+        super.onResume()
+    }
+
+    @SuppressLint("ServiceCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setSupportActionBar(toolbar)
+
+    }
+
+
+    override fun onStop() {
+        ContextCompat.startForegroundService(this, Intent(this, MyService::class.java))
+        super.onStop()
+    }
+
+}
+
+class MyService : Service(), SensorEventListener {
+
+    private var sensorManager: SensorManager? = null
+    private var running = false
+    private var totalStep = 0f
+    private var previousTotalStep = 0f
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        try {
+            running  =true
+            val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+            sensorManager?.registerListener(this,stepSensor, SensorManager.SENSOR_DELAY_UI)
+        } catch (e: Exception) {
+            Log.e("eee ERROR", e.message.toString())
+        }
+
+
+        return START_STICKY
+    }
+
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (running)
+            totalStep = event!!.values[0]
+        val currentSteps = totalStep.toInt() - previousTotalStep.toInt()
+        Constant.editor(this).putFloat(STEPNUMBER,previousTotalStep).apply()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun stopService(name: Intent?): Boolean {
+        return super.stopService(name)
+    }
+
+    override fun onDestroy() {
+        val intent = Intent(this, MyPhoneReciver::class.java)
+        sendBroadcast(intent)
+        super.onDestroy()
+    }
+
+}
+class MyPhoneReciver : BroadcastReceiver(){
+    override fun onReceive(context: Context?, intent: Intent?) {
+        ContextCompat.startForegroundService(context!!, Intent(context, MyService::class.java))
+    }
+
+}
